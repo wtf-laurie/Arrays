41efd6bba1381f073396172175e913a7fc57212e
Laurie B <42420692+wtf-laurie@users.noreply.github.com>
2018-12-14 11:11:35 -1000

    Add files via upload

---
+++ Exercise-2.js
@@ -0,0 +1,44 @@
+var myPenguin = {
+	name: 'Gunter',
+	realName: 'Orgalorg',
+	gender: 'Unsure',
+	origin: 'Adventure Time',
+	author: 'Pendelton Ward',
+	age: 'Unknown',
+};
+
+var selfIntro = "Hello, I'm a penguin and my name is Orgalorg, but people call me Gunter!";
+console.log(selfIntro);
+
+myPenguin.canFly = false;
+
+myPenguin.chirp = function() {
+	return "CHIRP CHIRP! Is this what penguins sound like?";
+};
+
+console.log(myPenguin.chirp());
+
+myPenguin.sayHello = function() {
+	return "Hello, I'm a penguin and my name is " + this.name;
+};
+
+console.log(myPenguin.sayHello());
+
+myPenguin.name = 'Penguin McPenguinFace';
+
+console.log(myPenguin.sayHello());
+
+myPenguin.fly = function() {
+	if (this.canFly == true) {
+		console.log("I can fly!");
+	}
+	else {
+		console.log("No flying for me!");
+	}
+};
+
+myPenguin.fly();
+
+myPenguin.canFly = true;
+
+myPenguin.fly();
